2184cd392d165eb86140d8aa08e06df34b805ef4
Michał <michmajchrzak@gmail.com>
2022-09-10 15:54:23 +0200

    fix migrations

--- db.scm
+++ db.scm
@@ -8,68 +8,119 @@
 
   (export 
     init-db
-    transaction
-    database
+    get-result-as-row
     conn-db
+    rows
+    database
     migrate)
 
   (begin
-
-    ; Database handler
     (define database (make-sqlite3))
 
     ; Function will get the DB details, connect to the DB using DB-Conn, remove everything from the DB and run DB create script
     (define (init-db db-handler)
      ; Check if there are migrations TABLE in the db
-     (let* ((table-names (sqlite:query db-handler "SELECT name FROM sqlite_schema WHERE type='table' ORDER BY name;"))
+     (let* ((table-names (sqlite:query db-handler "SELECT name FROM sqlite_master WHERE type='table' ORDER BY name;"))
             (are-migrations-there (member "migrations" table-names)))
       (if (not are-migrations-there)
-        (sqlite:query db-handler "CREATE TABLE \"migrations\" (
-          \"id\" INTEGER NOT NULL PRIMARY KEY,
-          \"created_at\" timestamp DEFAULT CURRENT_TIMESTAMP,
-          \"file_name\" TEXT NOT NULL,
-          \"number\" int NOT NULL);")
-       )))
-   
+        (begin
+          (print "Creating migration table!")
+          (sqlite:query db-handler "CREATE TABLE \"migrations\" (
+            \"id\" INTEGER PRIMARY KEY AUTOINCREMENT,
+            \"created_at\" timestamp DEFAULT CURRENT_TIMESTAMP,
+            \"file_name\" STRING NOT NULL);")
+       ))))
+  
+      (define (get-result-as-row statement)
+      (let ((n (sqlite3_column_count statement)))
+         (if (less? 0 n)
+            (let subloop ((i (- n 1)) (args '()))
+               (if (< i 0) args
+                  (subloop (- i 1) (cons
+                     (case (sqlite3_column_type statement i)
+                        (SQLITE_NULL    #false) ; should be #false or #null?
+                        (SQLITE_INTEGER (sqlite3_column_int statement i))
+                        (SQLITE_FLOAT   (sqlite3_column_double statement i))
+                        (SQLITE_TEXT    (sqlite3_column_text statement i))
+                        (SQLITE_BLOB    (syscall 9 ; mmap
+                           (sqlite3_column_blob statement i)
+                           (sqlite3_column_bytes statement i) 0))
+                        (else (error "Unsupported column type " i)))
+                     args)))))))
+    
+    (define (to-rows db-result to-vector?)
+      (case db-result
+         (#f #false) ; error
+         (#t #null)  ; no data available
+         (else
+            (define result 
+              (reverse
+                (let loop ((t '()))
+                  (let ((row (get-result-as-row db-result)))
+                    (if row
+                      (case (sqlite3_step db-result)
+                        (SQLITE_ROW
+                          (loop (cons row t)))
+                        (SQLITE_DONE
+                          (sqlite3_finalize db-result)
+                          (cons row t))
+                      (else
+                        (sqlite3_finalize db-result)
+                        (error "Can't execute SQL query"))))))))
+            (if to-vector?
+              (list->vector (map list->vector result))
+              result))))
 
-    ; Simple wrapper for TRANSACTIONS
-    (define (transaction query)
-      (begin
-        (sqlite:query "BEGIN TRANSACTION;")
-        (sqlite:query query)
-        (sqlite:query 
-         "commit;
-          rollback;")))
+    (define rows
+      (case-lambda
+        ((db-result) (to-rows db-result #false))
+        ((db-result to-vector?) (to-rows db-result #true))))
 
     ; Function will get the DB details and return the DB handler
-    (define (conn-db db-conn-details)
+    (define (conn-db database-handler db-conn-details)
      (let ((db-path (get db-conn-details "path" ":memory:")))
       (print "Connecting into database: " db-path)
-      (sqlite3_open db-path database)
-      (display " ...Connected!")))
+      (sqlite3_open db-path database-handler)
+      (display " ...Connected!\n")))
     
+    (define (insert-migration db-handler filename)
+      (sqlite:query db-handler "INSERT INTO migrations (file_name) VALUES (?);" filename))
+
     ; Function is reading file and running on the DB-HANDLER
-    (define (run-migration db-handler filename)
-      (let* ((file (open-input-file (string-append "./migrations/" filename)))
+    (define (run-migration db-handler filepath filename)
+     (print "FILENAME: " filename "\n")
+      (let* ((file (open-input-file (string-append filepath "/" filename)))
              (content (parse-string file)))
-        (if content
-          (transaction content)
-          (print "Can not open and process migration: " filename))
-        (close-port file)))
+        (begin 
+          (print "MIGRATION CONTENT: \n" content)
+          (close-port file)
+          (if content
+            (let* ((migration-result (sqlite:query db-handler content))
+                   (migration-result-rows (rows migration-result)))
+              (if migration-result
+                (insert-migration db-handler filename)
+                (print "Can not make migraiton from file " filename)))
+            (print "Can not open and process migration: " filename)))))
+
+    (define (make-migrations db-handler migrations-path)
+      (let* ((ls-path (or (get-environment-variable "LS_DIR") "/bin/ls"))
+              (migrations (make-cmd ls-path (list "ls" "-1" migrations-path) get-lines)))
+      (if (< (size migrations) 1)
+        (print "Can not find migrations at " migrations-path)
+        (let* ((sorted-migrations (sort (lambda (a b) (string<? (car a) (car b))) migrations))
+               (migrations-from-db (sqlite:query db-handler "select * from MIGRATIONS order by file_name DESC"))
+               (migrations-db-rows (rows migrations-from-db))
+               (migrations-names (map (lambda (elem)  (list-ref elem 2)) migrations-db-rows))
+               (filtered-migrations (filter (lambda (elem) (not (member (car elem) migrations-names))) sorted-migrations)))
+         
+         (if (> (length filtered-migrations) 0)
+          (map (lambda (elem) (run-migration db-handler migrations-path elem)) (car filtered-migrations))
+          '()) 
+         ))))
 
     ; Function will get all migrations from files and database. Compare them and run migrations which are not in the DB. 
-    (define (migrate db-handler)
-     ((let* ((ls-path (or (get-enviroment-variable "LS_DIR") "/bin/ls"))
-             (migrations (make-cmd ls-path (list "ls" "-1" "./migrations") get-lines)))
-      (if (< (size migrations) 1))
-        (print "Can not find and migrations.")
-        (let* ((sorted-migrations (sort string<? migrations))
-               (migrations-from-db (sqlite:query "select * form MIGRATIONS order by file_name DESC"))
-               (filtered-list (filter (lambda (elem) (not (member elem migrations-from-db))) sorted-migrations)))
-          
-         )
-
-       )))
-
-
+    (define migrate
+     (case-lambda
+      ((db-handler migrations-path) (make-migrations db-handler migrations-path))
+      ((db-handler) (make-migrations db-handler "./migrations"))))
    ))
--- test_db.scm
+++ test_db.scm
@@ -2,3 +2,77 @@
 
 (import (bubo db))
 (import (parsers parsers))
+(import (otus lisp))
+(import (lib sqlite))
+;(import (lib rlutil))
+
+(define GREEN "\e[0;32m")
+(define YELLOW "\e[0;33m")
+(define MAGENTA "\e[0;35m")
+(define END "\e[0;0m")
+
+(define (test-init-db)
+  (print END "Testing the DB initialization...")
+  (define db-handler database)
+  (begin
+   ; Connection to db in the memory
+   (conn-db db-handler #empty)
+   (init-db db-handler)
+   (let* ((result (sqlite:value db-handler "SELECT name FROM sqlite_master WHERE type='table' ORDER by name;'"))) 
+    (display MAGENTA)
+    (assert result ===> "migrations")
+    (print GREEN "...ok \n"))))
+
+(define (test-rows)
+  (define db-handler database)
+  (begin
+    (print END "Testing returning ROWS from DB...")
+    (conn-db db-handler #empty)
+    (init-db db-handler)
+    (sqlite:query db-handler "CREATE TABLE testRows (ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, NAME TEXT NOT NULL);")
+    (sqlite:query db-handler "INSERT INTO testRows (name) VALUES (\"Michal\");")
+    (sqlite:query db-handler "INSERT INTO testRows (name) VALUES (\"Henryk\");")
+    (sqlite:query db-handler "INSERT INTO testRows (name) VALUES (\"Dominik\");")
+    (let* ((result (sqlite:query db-handler "SELECT * FROM testRows where id > 0;"))
+           (result-rows (rows result)))
+     (display MAGENTA)
+     (assert (length result-rows) ===> 3)
+     (print GREEN "...ok\n"))))
+
+(define (test-migrate)
+  (print END "Testing DB migration system...")
+  (define db-handler database)
+  (begin
+    (conn-db db-handler #empty)
+    (init-db db-handler)
+    (let* ((temp (migrate db-handler "./test_migrations"))
+           (migrations-db (sqlite:query db-handler "SELECT name FROM sqlite_master where type='table' ORDER BY name;"))
+           (migrations-rows (rows migrations-db)))
+      (display MAGENTA)
+      (assert migrations-rows  ===> (list (list "migrations") (list "sqlite_sequence") (list "testTable") (list "testTable2")))
+      (print GREEN "...ok\n")
+      )))
+
+(define (test-migrate-no-dupes)
+  (print END "Testing DB migration system - no dupes...")
+  (define db-handler database)
+  (begin
+    (conn-db db-handler #empty)
+    (init-db db-handler)
+    (let* ((temp (migrate db-handler "./test_migrations"))
+           (temp2 (migrate db-handler "./test_migrations"))
+           (migrations-db (sqlite:query db-handler "SELECT name FROM sqlite_master where type='table' ORDER BY name;"))
+           (migrations-rows (rows migrations-db)))
+      (display MAGENTA)
+      (assert migrations-rows  ===> (list (list "migrations") (list "sqlite_sequence") (list "testTable") (list "testTable2")))
+      (print GREEN "...ok\n")
+      )))
+
+(define tests 
+ (list
+  test-rows
+  test-migrate
+  test-migrate-no-dupes
+  test-init-db))
+
+(map (lambda (test) (test)) tests)
